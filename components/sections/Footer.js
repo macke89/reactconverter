b0995a566c07f01437872c75e83dc48705fbe933
macke89 <mark.opp.1989@gmail.com>
2022-03-03 23:43:15 +0900

    Setting Up Sections

---
+++ components/sections/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="flex justify-between">
+            <div>copyright</div>
+            <div>powered by</div>
+        </footer>
+    );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,10 @@
+import Header from "/components/sections/Header"
+import Footer from "../components/sections/Footer";
+
 export default function Home() {
     return (<>
         <div className="min-h-screen flex flex-col justify-between">
-            <header>
-                <div className="navbar bg-base-100">
-                    <div className="flex-1">
-                        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-                    </div>
-                    <div className="flex-none">
-                        <ul className="menu menu-horizontal p-0">
-                            <li><a>Item 1</a></li>
-                            <li tabIndex="0">
-                                <a>
-                                    Parent
-                                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20"
-                                         height="20" viewBox="0 0 24 24">
-                                        <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/>
-                                    </svg>
-                                </a>
-                                <ul className="p-2 bg-base-100">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </header>
+            <Header />
             <main className="flex justify-center w-full">
                 <div>
                     <div className="btn-group mb-10">
@@ -53,10 +31,7 @@ export default function Home() {
                     </div>
                 </div>
             </main>
-            <footer className="flex justify-between">
-                <div>copyright</div>
-                <div>powered by</div>
-            </footer>
+            <Footer/>
         </div>
     </>)
 }
