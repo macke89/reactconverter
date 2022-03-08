0dc6502f2293dd5b0374a631d86fe0af892f427c
macke89 <mark.opp.1989@gmail.com>
2022-03-08 22:01:43 +0900

    Fixed Temperatures

--- components/InputC.js
+++ components/InputC.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-const InputC = ({name, value, callback}) => {
-    const change = event => {
-        value = event.target.value
-        callback(value)
-    }
+const InputC = ({value, onChange, label}) => {
     return (
-        <label className="input-group" key={id}>
-            <span className="w-1/4">{name}</span>
-            <input type="number" placeholder={name}
+        <label className="input-group" key={label}>
+            <span className="w-1/4">{label}</span>
+            <input type="number" placeholder={label}
                    className="input input-bordered input-xs w-3/4 max-w-xs"
                    value={value}
-                   onChange={callback}
+                   onChange={onChange}
                    step={1}
             />
         </label>
--- components/sections/Temperatures.js
+++ components/sections/Temperatures.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import Input from "../Input";
 import InputC from "../InputC";
 
+// TODO Fix this
+
 const temperatures = [
     {name: "fahrenheit", rate: 1},
     {name: "celsuis", rate: 100},
@@ -9,24 +11,33 @@ const temperatures = [
 ]
 
 const Temperatures = () => {
-    const [nominator, setNominator] = useState(2);
+    const [celsius, setCelsius] = useState(0);
+    const [fahrenheit, setFahrenheit] = useState(0);
+    const [kelvin, setKelvin] = useState(0);
 
-    function callback(nominator) {
-        setNominator(nominator)
+    const handleCelsius = event => {
+        setCelsius(event.target.value);
+        setFahrenheit(parseFloat(((event.target.value * 9 / 5) + 32).toFixed(5)));
+        setKelvin(parseFloat((parseInt(event.target.value) + 273.15).toFixed(5)));
     }
 
-    function calcF(nominator) {
-        return nominator;
+    const handleFahrenheit = event => {
+        setFahrenheit(event.target.value);
+        setCelsius(parseFloat(((event.target.value - 32) * 5 / 9).toFixed(5)));
+        setKelvin(parseFloat(((event.target.value - 32) * 5 / 9 + 273.15).toFixed(5)));
     }
 
-    function calcC(nominator) {
-        return nominator * 2;
+    const handleKelvin = event => {
+        setKelvin(event.target.value);
+        setCelsius(parseFloat((event.target.value - 273.15).toFixed(5)));
+        setFahrenheit(parseFloat(((event.target.value - 273.15) * 9 / 5 + 32).toFixed(5)))
     }
 
     return (
         <div className="form-control w-full flex flex-col gap-5 mb-2">
-            <Input name="fahrenheit" value={calcF(nominator)} callback={callback}/>
-            <Input name="fahrenheit" value={calcF(nominator)} callback={callback}/>
+            <InputC label="Celsius" value={celsius} onChange={handleCelsius}/>
+            <InputC label="Fahrenheit" value={fahrenheit} onChange={handleFahrenheit}/>
+            <InputC label="Kelvin" value={kelvin} onChange={handleKelvin}/>
         </div>
     );
 };
