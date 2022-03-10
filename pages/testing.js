a6352364e8abc534290eeb415a01b0634716c1dd
macke89 <mark.opp.1989@gmail.com>
2022-03-10 22:22:25 +0900

    Changed to SCSS

--- pages/testing.js
+++ pages/testing.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Testing = () => {
     return (
         <div className="flex w-screen justify-center gap-10 mt-10">
-            <button className="bg-red-300">Box 1</button>
-            <button className="mo-btn-primary">Box 2</button>
+            <button className="btn btn1">Box 1</button>
+            <button className="btn">Box 2</button>
         </div>
     );
 };
