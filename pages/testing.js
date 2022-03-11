4d8561a3e2ac36726ff54d467e232a6cf6dffbe7
macke89 <mark.opp.1989@gmail.com>
2022-03-11 23:58:28 +0900

    SCSS Animation Testing x 2

--- pages/testing.js
+++ pages/testing.js
@@ -5,6 +5,8 @@ const Testing = () => {
         <div className="flex w-screen justify-center gap-10 mt-10">
             <button className="btn btn1">Box 1</button>
             <button className="btn btn2">Box 2</button>
+            <button className="btn btn3">Box 3</button>
+            <button className="btn btn4">Box 4</button>
         </div>
     );
 };
