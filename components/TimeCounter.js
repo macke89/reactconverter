c052448630a7e25c045d62387d6579c3be37cf00
macke89 <mark.opp.1989@gmail.com>
2022-03-17 22:17:42 +0900

    TimeCounter.js Setup

--- components/TimeCounter.js
+++ components/TimeCounter.js
@@ -1,17 +1,25 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+
+const times = (nominator) => {
+    let arr = []
+    arr[0] = nominator
+    arr[1] = 2
+    arr[2] = 2
+    arr[3] = 2
+    arr[4] = 2
+    return arr
+}
 
 const TimeCounter = ({nominator}) => {
-    const times = (nominator) => {
-        let arr = []
-        arr[0] = nominator
-        return arr
-    }
-
-    times(nominator)
+    let variables = times(nominator)
     return (
         <div>
             <h1>Nominator: {nominator}</h1>
-            <p>Weeks: {times[0]}</p>
+            <div>{variables[0]} weeks</div>
+            <div>{variables[1]} days</div>
+            <div>{variables[2]} hours</div>
+            <div>{variables[3]} minutes</div>
+            <div>{variables[4]} seconds</div>
         </div>
     );
 };
