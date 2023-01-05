c345c2c72cd6aba4a6e672a72a33c6b9674f546a
macke89 <mark.opp.1989@gmail.com>
2023-01-06 08:50:17 +0900

    Style Changes

--- components/InputC.js
+++ components/InputC.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 const InputC = ({value, onChange, label}) => {
+    const handleFocus = (event) => event.target.select();
+
     return (
         <label className="input-group input-group-vertical items-center">
             <span
                 className="w-56 capitalize hover:cursor-pointer group-focus:bg-red-300 font-semibold py-2 btn-primary transition-all duration-500 hover:text-white">
                 {label}
             </span>
-            <input type="number" placeholder={label}
+            <input type="number"
+                   placeholder={label}
                    className="w-56 input input-bordered input max-w-xs"
                    value={value}
                    onChange={onChange}
                    step={1}
+                   onFocus={handleFocus}
             />
         </label>
     );
--- components/sections/Footer.js
+++ components/sections/Footer.js
@@ -6,10 +6,15 @@ const Footer = () => {
     const year = dateObj.getUTCFullYear(); //months from 1-12
 
     return (
-        <footer className="bg-base-200 p-2 flex items-center justify-center gap-10">
-            {/*<Link href="https://moweb.digital/" passHref>*/}
-            {/*    <a target="_blank" className="btn m-1 btn-secondary font-bold transition-all duration-300">Homepage</a>*/}
-            {/*</Link>*/}
+        <footer className="bg-base-200 p-2 flex flex-col items-center justify-center gap-1 sm:flex-row">
+            <div>
+                <Link href="https://e-girls.art//" passHref>
+                    <a target="_blank" className="btn m-1 btn-secondary font-bold transition-all duration-300">E-Girls</a>
+                </Link>
+                <Link href="http://promptgen.online//" passHref>
+                    <a target="_blank" className="btn m-1 btn-secondary font-bold transition-all duration-300">Prompt Generator</a>
+                </Link>
+            </div>
             <p className="font-semibold">
                 Copyright © {year} convertall.cc
             </p>
